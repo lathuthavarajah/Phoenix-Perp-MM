04f956ecc9094a8565ac99f3dabd65530d6ae933
Lathu Thavarajah <lathuthavarajah@gmail.com>
2026-03-15 17:07:56 -0400

    Reset: remove all files for fresh start

--- dashboard/lib/funding-api.ts
+++
@@ -1,7 +0,0 @@
-import { FundingRateResponse } from "./types";
-
-export async function fetchFundingRates(): Promise<FundingRateResponse> {
-  const res = await fetch("/api/funding", { cache: "no-store" });
-  if (!res.ok) throw new Error(`Funding API error: ${res.status}`);
-  return res.json();
-}
--- engine/src/tests/mod.rs
+++
@@ -1,3 +0,0 @@
-mod hedge_engine_tests;
-mod risk_monitor_tests;
-mod funding_monitor_tests;
